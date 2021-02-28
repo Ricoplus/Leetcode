7e5903151e0d1f41a27193358821e8cac1e2bb76
rico <759249230@qq.com>
2021-02-28 18:03:08 +0800

    1

--- 1.两数之和.go
+++ 1.两数之和.go
@@ -6,16 +6,36 @@
 
 // @lc code=start
 func twoSum(nums []int, target int) []int {
-	sli := []int{}
-	for i := 0; i<len(nums);i++{
-		for j:=i+1;j<len(nums);j++{
-			if nums[i]+ nums[j] == target{
-				sli = append(sli,i)
-				sli = append(sli,j)
-			}
+
+	// 1.暴力解法
+	// 复杂度分析
+	// 空间复杂度O(1)
+	// 时间复杂度O(N^2)
+	// for i := 0; i < len(nums); i++ {
+	// 	for j := i + 1; j < len(nums); j++ {
+	// 		if nums[i]+nums[j] == target {
+	// 			return []int{i, j}
+	// 		}
+	// 	}
+	// }
+
+	// 2.哈希表
+	// 备注下：哈希表解法，第一次访问也许会错过，但是当顺序遍历到后边时，
+	// 又会访问到之前的元素，所以不必担心刚开始hashtable没有而导致不能够组合。
+	// 复杂度分析
+	// 时间复杂度：O(N)
+	// 空间复杂度：O(N)
+	hashtable := make(map[int]int)
+	for i, _ := range nums {
+		// 这句话保证不会有重复元素
+		another := target - nums[i]
+		if _, ok := hashtable[another]; ok {
+			return []int{i, hashtable[another]}
 		}
+		hashtable[nums[i]] = i
 	}
-	return sli
+	return nil
 }
+
 // @lc code=end
 
